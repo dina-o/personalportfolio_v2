fd0d878a421c48250f26eec69e6f95a6ff88e302
Dina Orucevic <dina.oorucevic@gmail.com>
2023-07-21 00:24:08 -0400

    work experiences section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import {Home} from './components/home'
 import {About} from './components/about'
+import {Work} from './components/work'
 
 function App() {
   return (
    <>
       <Home />
       <About />
+      <Work />
    </>
   );
 }
---
+++ src/components/work.jsx
@@ -0,0 +1,100 @@
+import { Text, VStack, HStack } from '@chakra-ui/react';
+import image3 from '../images/overlay3.jpg'
+import image2 from '../images/overlay2.jpg'
+import image1 from '../images/overlay1.jpg'
+
+const Work = () => {
+  return (
+    <>
+      <div id="work"
+        style={{
+          height: '100vh',
+          width: "100%",
+          overflow: 'hidden',
+        }}
+      >   
+
+      <HStack>
+
+        <div className="item">
+          <div className="item-wrapper">
+            <img src={image1}></img>
+            <VStack style={{fontFamily: "CanelaRegItalic"}}>
+              <div className="back-text">
+                <Text className="after-overlay" style={{fontSize: "5vh"}}>Designed and developed end to end proof of concepts for Visa's Pay by Bank feature and Interac.</Text>
+              </div>
+              <div className="bottom-text">
+                <Text className="after-overlay"  style={{fontSize: "6vh", color: "#6A737E"}}>✷✷✷</Text>
+                <br />
+                <Text className="after-overlay" style={{fontSize: "4vh", paddingLeft: "20px" }}>React JS / TypeScript / Node.js / MongoDB</Text>
+              </div>
+            </VStack>
+            <div className="img-overlay">
+                <VStack className="item-name">
+                  <VStack alignItems="left" className="front-text">
+                    <Text style={{paddingLeft: "15%", fontSize: "7vh"}}>Payments Innovation Developer </Text>
+                    <Text style={{color: "black", fontSize: "4vh", paddingLeft: "15%"}}>TD Bank</Text>
+                  </VStack>
+                  <Text className="id">1</Text>
+                </VStack>
+            </div>
+          </div>
+        </div>
+
+        <div className="item">
+          <div className="item-wrapper">
+            <img src={image2}></img>
+            <VStack style={{fontFamily: "CanelaRegItalic"}}>
+              <div className="back-text">
+                <Text className="after-overlay" style={{fontSize: "5vh"}}>Worked on navigation and authoritative content features for bank employees' coaching dashboards.</Text>
+              </div>
+              <div className="bottom-text">
+                <Text className="after-overlay"  style={{fontSize: "6vh", color: "#6A737E"}}>✷✷✷</Text>
+                <br />
+                <Text className="after-overlay" style={{fontSize: "4vh", paddingLeft: "20px" }}>Angular JS, TypeScript, Cinchy DB, SQL</Text>
+              </div>
+            </VStack>
+            <div className="img-overlay">
+                <VStack className="item-name">
+                  <VStack alignItems="left" className="front-text">
+                    <Text style={{paddingLeft: "15%", fontSize: "7vh"}}>Software Engineer </Text>
+                    <Text style={{color: "black", fontSize: "4vh", paddingLeft: "15%"}}>TD Bank</Text>
+                  </VStack>
+                  <Text className="id">2</Text>
+                </VStack>
+            </div>
+          </div>
+        </div>
+
+        <div className="item">
+          <div className="item-wrapper">
+            <img src={image3}></img>
+            <VStack style={{fontFamily: "CanelaRegItalic"}}>
+              <div className="back-text">
+                <Text className="after-overlay" style={{fontSize: "5vh"}}>current internship...</Text>
+              </div>
+              <div className="bottom-text">
+                <Text className="after-overlay"  style={{fontSize: "6vh", color: "#6A737E"}}>✷✷✷</Text>
+                <br />
+                <Text className="after-overlay" style={{fontSize: "4vh", paddingLeft: "20px" }}>React JS, TypeScript, Chakra UI, Temporal</Text>
+              </div>
+            </VStack>
+            <div className="img-overlay">
+                <VStack className="item-name">
+                  <VStack alignItems="left" className="front-text">
+                    <Text style={{paddingLeft: "15%", fontSize: "7vh"}}>Full Stack Developer </Text>
+                    <Text style={{color: "black", fontSize: "4vh", paddingLeft: "15%"}}>RFA Labs</Text>
+                  </VStack>
+                  <Text className="id">3</Text>
+                </VStack>
+            </div>
+          </div>
+        </div>
+        
+        </HStack>
+        </div>  
+    </>
+  );
+};
+
+export { Work };
