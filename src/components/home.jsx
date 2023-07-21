776eb5e912b8c94c9defc24facf774d6320ae99d
Dina Orucevic <dina.oorucevic@gmail.com>
2023-07-21 00:05:12 -0400

    hero text for home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -27,6 +27,33 @@ import {Link} from 'react-scroll'
          <Link to="contact" spy={true} smooth={true} offset={50} duration={500} className= "menu"> <a className= "values"> contact </a> </Link>
       </HStack>
    </Flex>
+
+   <Box width="95%" style={{
+       marginLeft: "auto",
+       marginRight: "auto"
+   }}>
+      <Divider borderWidth="1px" className='divider' style={{opacity: "0.5"}} />
+   </Box>
+
+   <Heading style={{
+            paddingTop: "8%",
+            paddingLeft: "5%",
+            fontWeight: 'lighter',
+         }}
+      fontFamily="CanelaThinItalic" fontSize='7vh'>creative</Heading>
+
+   <Heading style={{paddingLeft: "5%"}} fontFamily="CanelaThinItalic" fontSize='13vh'>DEVELOPER <a style={{ fontSize: "4vh", color: '#AFD8BB'}}>✷</a></Heading>
+
+
+   <Text className="blurb" fontFamily="CanelaThinItalic" fontSize='4vh' style={{marginTop: "-9%", float: "right", paddingRight: "8%", paddingLeft: "60%"}}>
+      &emsp;&emsp;&emsp;a passionate frontend / web developer with an eye for impressionism and a goal to create more elegant  products in the technology industry.
+      </Text>
+
+      <Link class="circle button" to="about">
+         <span></span>
+         <span></span>
+         <span></span>
+      </Link>
    
 
     </div>
